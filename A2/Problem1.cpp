b4df7e57ad3466fdd7d6492c21edbf0dc25fc23b
dozothebozo <dozothebozo@gmail.com>
2026-04-16 18:20:44 -0400

    add(A2): template

---
+++ A2/Problem1.cpp
@@ -0,0 +1,24 @@
+// File Name: Problem1.cpp
+// Project: CS 170 Assignment 2 Problem 1
+// Author: Gogi Benny
+// Date: Monday, April 16, 2026
+//
+// Purpose:
+//
+// Major Items:
+// -
+// -
+// -
+
+#include <iostream>
+
+using namespace std;
+
+// function prototypes
+
+// global declarations of constants
+
+int main()
+{
+    cout << "Hello World\n";
+}
